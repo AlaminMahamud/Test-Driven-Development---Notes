dee95c22b9dcc54b458de0ae2468fbbf2031ab42
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2016-02-12 10:59:05 +0600

    Create Assertions with JUnit Framework.java

---
+++ Lynda.com/Assertions with JUnit Framework.java
@@ -0,0 +1,11 @@
+assertEquals(arg1, arg2);
+
+  assertEquals(1,2); // fail, error
+  assertEquals(1,1); // pass, do nothing
+
+assertArrayEquals(array1, array2);
+assertNotNull(obj);
+assertNull(obj);
+assertSame(obj1, obj2);
+
+assertNotSame(obj1,obj2);
